32a78a8e7bd048467b7e64031385ca0f83136c68
MalavyaRaval <malavyaraval@gmail.com>
2025-11-29 16:05:30 -0800

    server working with chat in vs copilot

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,80 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import fs from "node:fs/promises";
 
-const server = new McpServer({});
+const server = new McpServer({
+    name: "My MCP Server",
+    version: "1.0.0",
+})
+
+server.tool("create-user", "Create a new user in the database", {
+    name : z.string(),
+    email: z.string(),
+    address: z.string(),
+    phone: z.string()
+
+},{
+    title: "Create User",    // next 4 lines and this line is optional
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+
+}, async (params) => {
+    try {
+        const id = await createUser(params)
+        return {
+            content: [
+                {
+                    type: 'text', text: `User ${id} created successfully`
+                }
+            ]
+        }
+    }
+    catch (error) {
+        return {
+            content: [
+                {
+                    type: 'text', text: "Failed to save user"
+                }
+            ]
+        }
+    }
+})
+
+async function createUser(user: { name: string; email: string; address: string; phone: string })
+{
+    
+    const users = await import ("./data/users.json", {
+        with: {
+            type: "json"
+        }
+    }).then(m => m.default)
+
+    const id = users.length + 1
+
+    users.push({id, ...user})
+
+    await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 2))
+
+    return id
+}
+
+
+
+
+
+
+
+
+
+
+
+
+async function main() {
+    const transport = new StdioServerTransport()
+    await server.connect(transport)
+}
+
+main()
